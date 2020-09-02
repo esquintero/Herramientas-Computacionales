df574ca1cacf4411b9ff4a1ae701916bc33e858a
Esteban Quintero <esquintero@unal.edu.co>
2020-09-02 12:25:05 -0500

    ahora va one y eps

--- clase0209/overflow.cpp
+++ clase0209/overflow.cpp
@@ -2,15 +2,15 @@
 #include<cstdlib>
 int main(int argc, char **argv)
 {
-	std::cout.precision(15);
+	std::cout.precision(7);
 	std::cout.setf(std::ios::scientific);
-	double under = 1.0;
-	double over = 1.0;
+	float eps = 1.0;
+	float one = 1.0;
 	int N = std::atoi(argv[1]);
 	for(int ii = 0; ii <= N; ii++){
-		under = under / 2.0;
-		over = over * 2.0;
-		std::cout<< ii << " "<< under << " " << over << "\n";
+		eps = eps / 2.0;
+		one = 1.0 + eps;
+		std::cout<< ii << " "<< one << " " << eps << "\n";
 	}
 	return 0;
 }
